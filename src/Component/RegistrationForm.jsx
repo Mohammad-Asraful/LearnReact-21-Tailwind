9141d710f93bae8064c09bf77074d4a4c96bf990
Mohammad-Asraful <asrafmail01@gmail.com>
2023-02-20 21:01:42 +0600

    change input text color

--- src/Component/RegistrationForm.jsx
+++ src/Component/RegistrationForm.jsx
@@ -11,10 +11,10 @@ const RegistrationForm = () => {
                <h1 className='uppercase text-white mb-9'>Sign in</h1>
                <form>
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="name">User Name</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="text" name="text" placeholder='Enter your name' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="text" name="text" placeholder='Enter your name' />
 
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="password">Password</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
 
                   <input className='mt-2' type="checkbox" /> <span className='text-white'> Keep me signed in</span>
 
@@ -29,16 +29,16 @@ const RegistrationForm = () => {
                <h1 className='uppercase text-white mb-9'>Sign up</h1>
                <form>
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="name">User Name</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="text" name="text" placeholder='Enter your name' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="text" name="text" placeholder='Enter your name' />
 
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="password">Password</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
 
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="password">Repeat Password</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="password" name="password" placeholder='Enter your password' />
 
                   <label className='block text-white opacity-50 uppercase mb-1' htmlFor="email">Email Address</label>
-                  <input className='py-1 px-2 block text-black w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="email" name="email" placeholder='Enter your email' />
+                  <input className='py-1 px-2 block text-white w-[50vh] lg:w-[400px] rounded-full bg-transparent border-2 mb-2' type="email" name="email" placeholder='Enter your email' />
 
                   <button className='block text-white bg-blue-600 uppercase rounded-full py-1 px-2 mt-10 w-[50vh] lg:w-[400px] text-center'>Sign in</button>
                </form>
